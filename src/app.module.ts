816899e4aa25eb507e674239100b9f750cebd5a0
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-12-06 19:14:39 -0300

    chore: change baseUrl on tsconfig file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { databaseConfig } from './main/config';
+import { databaseConfig } from '@/main/config';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 
